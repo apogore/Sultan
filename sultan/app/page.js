ee847b04a2c312c21386e4010274dafffc66b68f
Aleksandr Pogorelov <apogore@sfedu.ru>
2024-10-13 18:52:10 +0300

    Создан скелет сайта
    Добавил шрифт вёрстки, разграничил шапку, секции и футер в главной. Написал комментарии для дальнейшей работы

--- sultan/app/page.js
+++ sultan/app/page.js
@@ -1,95 +1,43 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import './page.css'; // Импорт стилей
+import Header from './shared/header/header'; // Импорт компонента заголовка
+import Footer from './shared/footer/footer'; 
+import MiniCard from './shared/mini-card/mini-card';
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const HomePage = () => {
+    return (
+        <div className="grid-container">
+            <div className="spacer" />
+            <div className="content">
+              <Header className="header" />
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+              <div className='body'>
+                <div className='section banner'>
+                  <h3>Баннер с фоткой</h3>
+                </div>
+                <div className='section cards'> 
+                  <h3>Акционные товары</h3>
+                  <MiniCard className="mini-card" />
+                </div>
+                <div className='section category'> 
+                  <h3>Категории товаров</h3>
+                </div>
+                <div className='section carousel'> 
+                  <h3>Карусель акций</h3>
+                </div>
+                <div className='section best'>
+                  <h3>Лучшие товары</h3>
+                </div>
+                <div className='section map'>
+                  <h3>Геолокация</h3>
+                </div>
+              </div>
+              
+              <Footer className="footer" />
+
+            </div>
+            <div className="spacer" />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+    );
+};
+
+export default HomePage;
