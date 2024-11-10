c8764cbc356f185028e5a2e9e4bc0c71ad53772c
Екатерина Бочарова <syntynytkn@gmail.com>
2024-11-10 20:18:41 +0300

    feature: сделан адаптив хедера

---
+++ sultan/app/shared/navigation-links.js
@@ -0,0 +1,16 @@
+export const navigationLinks = [{
+    ref: '/about', // Используется в JSX в качестве ключа, значение должно быть уникальным
+    name: 'О компании',
+  },
+  {
+    ref: '/delivery',
+    name: 'Доставка и оплата',
+  },
+  {
+    ref: '/return',
+    name: 'Возврат',
+  },
+  {
+    ref: '/contacts',
+    name: 'Контакты',
+  }]
