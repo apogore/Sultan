b4022440d72dbb610ea530d647fb9385377bd8e9
Aleksandr Pogorelov <apogore@sfedu.ru>
2024-11-16 20:47:42 +0300

    fix layout

--- sultan/app/layout.js
+++ sultan/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import ".styles/globals.css";
+import "./styles/globals.css";
 import Header from "./shared/header/header";
 import Footer from "./shared/footer/footer";
 const inter = Inter({
