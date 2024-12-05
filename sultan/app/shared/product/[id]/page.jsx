1da484bd8551c4eee4ec37eb0677becd411788a8
Екатерина Бочарова <syntynytkn@gmail.com>
2024-12-05 13:56:46 +0300

    fix: отступы и импорты

--- sultan/app/shared/product/[id]/page.jsx
+++ sultan/app/shared/product/[id]/page.jsx
@@ -2,11 +2,9 @@
 import React, { useEffect, useState } from "react";
 import ProductList from "@shared/product-list/product-list.jsx";
 import getPriceList from "@functions/price-list";
-import ProductInfo from "@functions/product-info";
 import Button from "@ui/button/button";
 import Accordion from "@ui/accordion/accordion";
 import "./page.scss";
-import "@ui/button/button.scss";
 
 const ProductPage = ({ params }) => {
   const { id } = params;
