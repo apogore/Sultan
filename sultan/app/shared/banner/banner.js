0221634dfe6f4a9a6cff2ac6501f517fad6b0b11
DIMas821k <dmitrij.zubckov2014@yandex.ru>
2024-12-12 19:14:59 +0300

    fix-import

--- sultan/app/shared/banner/banner.js
+++ sultan/app/shared/banner/banner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "@ui/button/button";
 import "./banner.scss";
-import Button from "../../ui/button/button";
+
 
 const Banner = () => {
   const handleButtonClick = () => {
