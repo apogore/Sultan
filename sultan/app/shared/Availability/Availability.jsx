94523208b6e1c6be232266a9ea2c995bbfb98a76
Fornar <114926932+apogore@users.noreply.github.com>
2024-12-26 13:55:43 +0300

    Update Availability.jsx

--- sultan/app/shared/Availability/Availability.jsx
+++ sultan/app/shared/Availability/Availability.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Availability = ({ amount }) => {
   const availabilityClass = amount > 0 ? "in-stock" : "out-of-stock";
   const availabilityText = amount > 0 ? "В наличии" : "Нет в наличии";
-
   return <div className={`available ${availabilityClass}`}>{availabilityText}</div>;
 };
 
